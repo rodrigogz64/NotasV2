74e1e023433c61fa8e063064a46d5df690bf4f89
Alejandro Monge <almartinez0707@gmail.com>
2022-04-22 02:09:27 -0600

    Set classes in one folder

---
+++ src/Clases/ListaProfesor.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Conexion;
+
+class ListaProfesor extends Conexion
+{
+    //metodo que muestra a todos los profesores
+    public function verLista($rol){
+        //conexion DB
+        $this->conexionDB();
+
+        $query = "SELECT profesor.*, materia.nombre AS materia, estado.tipoEstado AS estado FROM profesor INNER JOIN materia on profesor.Idmateria=materia.id INNER JOIN estado ON profesor.idEstado=estado.Id WHERE profesor.idEstado=$rol";
+        $result = mysqli_query($this->con, $query);
+        $cont = 1;
+        while($imp = mysqli_fetch_array($result)){
+            $tabla = "<tr>";
+                $tabla .= "<td>".$cont."</td>";
+                $tabla .= "<td>".$imp['Nombre']."</td>";
+                $tabla .= "<td>". $imp['Apellido'] ."</td>";
+                $tabla .= "<td>". $imp['Edad'] ."</td>";
+                $tabla .= "<td>". $imp['materia'] ."</td>";
+                $tabla .= "<td>". $imp['Usuario'] ."</td>";
+                $tabla .= "<td>". $imp['estado'] ."</td>";
+                $tabla .= "<form action='modificarProfesor.php' method='POST'>";
+                    $tabla .= "<td><button name='idprofesor' value='".$imp['Id']."'>Modificar</button></td>";
+                $tabla .= "</form>";
+            $tabla .= "</tr>";
+            echo $tabla;
+            $cont++;
+            
+        }
+    }
+    
+    //metodo que muestra a todos los profesores Inactivos
+    public function listaProfesorInactivos(){
+        $this->listaProfesor(2);
+    }
+}
